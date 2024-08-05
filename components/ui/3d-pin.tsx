52d149fbe8d10c9c37ff59b5f92b141d89e9d6e8
Dylan Di Filippo <dylan.di.filippo@gmail.com>
2024-08-05 23:28:36 +0200

    chore: Update RecentProjects component and 3D pin styling

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React, { useState } from 'react'
 import { motion } from 'framer-motion'
-
 import { cn } from '@/lib/utils'
 
 export const PinContainer = ({
@@ -43,8 +42,7 @@ export const PinContainer = ({
           style={{
             transform: transform,
           }}
-          // remove  bg-black
-          className='absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden'
+          className='absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] bg-black border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden'
         >
           <div className={cn(' relative z-50 ', className)}>{children}</div>
         </div>
@@ -59,8 +57,7 @@ export const PinContainer = ({
 
 export const PinPerspective = ({ title, href }: { title?: string; href?: string }) => {
   return (
-    // change w-96 to w-full
-    <motion.div className='pointer-events-none w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500'>
+    <motion.div className='pointer-events-none  w-96 h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500'>
       <div className=' w-full h-full -mt-7 flex-none  inset-0'>
         <div className='absolute top-0 inset-x-0  flex justify-center'>
           <a
