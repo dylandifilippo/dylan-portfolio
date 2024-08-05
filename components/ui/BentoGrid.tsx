3143e3f2d04fcd32f718ef63c9dcbe56f70f831d
Dylan Di Filippo <dylan.di.filippo@gmail.com>
2024-08-05 23:18:46 +0200

    chore: Add RecentProjects component to home page

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { IoCopyOutline } from 'react-icons/io5'
 import Lottie from 'react-lottie'
 import { cn } from '@/lib/utils'
@@ -11,11 +11,7 @@ import MagicButton from '../MagicButton'
 export const BentoGrid = ({ className, children }: { className?: string; children?: React.ReactNode }) => {
   return (
     <div
-      className={cn(
-        // change gap-4 to gap-8, change grid-cols-3 to grid-cols-5, remove md:auto-rows-[18rem], add responsive code
-        'grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto',
-        className
-      )}
+      className={cn('grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto', className)}
     >
       {children}
     </div>
@@ -27,7 +23,6 @@ export const BentoGridItem = ({
   id,
   title,
   description,
-  //   remove unecessary things here
   img,
   imgClassName,
   titleClassName,
@@ -42,10 +37,16 @@ export const BentoGridItem = ({
   titleClassName?: string
   spareImg?: string
 }) => {
-  const leftLists = ['ReactJS', 'Express', 'Typescript']
-  const rightLists = ['VueJS', 'NuxtJS', 'GraphQL']
+  const leftLists = ['ReactJS', 'Next.js', 'Typescript']
+  const rightLists = ['Node.js', 'Express', 'GraphQL']
 
   const [copied, setCopied] = useState(false)
+
+  const handleCopy = () => {
+    const text = 'dylan.di.filippo@gmail.com'
+    navigator.clipboard.writeText(text)
+    setCopied(true)
+  }
 
   const defaultOptions = {
     loop: copied,
@@ -56,22 +57,13 @@ export const BentoGridItem = ({
     },
   }
 
-  const handleCopy = () => {
-    const text = 'hsu@jsmastery.pro'
-    navigator.clipboard.writeText(text)
-    setCopied(true)
-  }
-
   return (
     <div
       className={cn(
-        // remove p-4 rounded-3xl dark:bg-black dark:border-white/[0.2] bg-white  border border-transparent, add border border-white/[0.1] overflow-hidden relative
         'row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4',
         className
       )}
       style={{
-        //   add these two
-        //   you can generate the color from here https://cssgradient.io/
         background: 'rgb(4,7,29)',
         backgroundColor: 'linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)',
       }}
@@ -98,7 +90,6 @@ export const BentoGridItem = ({
           )}
         </div>
         {id === 6 && (
-          // add background animation , remove the p tag
           <BackgroundGradientAnimation>
             <div className='absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl'></div>
           </BackgroundGradientAnimation>
@@ -110,12 +101,10 @@ export const BentoGridItem = ({
             'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
           )}
         >
-          {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
           <div className='font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10'>
             {description}
           </div>
-          {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
-          {/* remove mb-2 mt-2 */}
+
           <div className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}>{title}</div>
 
           {/* for the github 3d globe */}
@@ -153,12 +142,7 @@ export const BentoGridItem = ({
           )}
           {id === 6 && (
             <div className='mt-5 relative'>
-              {/* button border magic from tailwind css buttons  */}
-              {/* add rounded-md h-8 md:h-8, remove rounded-full */}
-              {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
-              {/* add handleCopy() for the copy the text */}
               <div className={`absolute -bottom-5 right-0 ${copied ? 'block' : 'block'}`}>
-                {/* <img src="/confetti.gif" alt="confetti" /> */}
                 <Lottie
                   options={defaultOptions}
                   height={200}
