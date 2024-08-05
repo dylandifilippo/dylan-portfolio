3544b0f6fa76e03c4b56a597eb14cb01a2f98d29
Dylan Di Filippo <dylan.di.filippo@gmail.com>
2024-08-05 14:48:25 +0200

    Create the grid section

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
-import Hero from '@/components/Hero'
-import { FloatingNav } from '@/components/ui/FloatingNav'
+'use client'
 
-export default function Home() {
+import { navItems } from '@/data'
+
+import Hero from '@/components/Hero'
+import Grid from '@/components/Grid'
+import { FloatingNav } from '@/components/ui/FloatingNavbar'
+
+const Home = () => {
   return (
     <main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
       <div className='max-w-7xl w-full'>
-        <FloatingNav
-          navItems={[
-            { name: 'Home', link: '#' },
-            { name: 'About', link: '#about' },
-            { name: 'Projects', link: '#projects' },
-            { name: 'Contact', link: '#contact' },
-          ]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
+        <Grid />
       </div>
     </main>
   )
 }
+
+export default Home
