e4cc173f72d883f1a281532b77c3a5bf8e6e9d16
Dylan Di Filippo <dylan.di.filippo@gmail.com>
2024-08-08 15:15:15 +0200

    chore: Update Footer component to include email and LinkedIn buttons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,20 +21,23 @@ const Footer = () => {
         <p className='text-white-200 md:mt-10 my-5 text-center'>
           Reach out to me today and let&apos;s discuss how I can help you achieve your goals.
         </p>
-        <a href='mailto:dylan.di.filippo@gmail.com'>
-          <MagicButton
-            title='Send me an email'
-            icon={<MdOutlineEmail />}
-            position='right'
-          />
-        </a>
-        <a href='https://www.linkedin.com/in/dylandifilippo/'>
-          <MagicButton
-            title='Connect with me on LinkedIn'
-            icon={<FaLinkedin />}
-            position='right'
-          />
-        </a>
+
+        <div className='flex flex-col justify-center gap-2 md:gap-0'>
+          <a href='mailto:dylan.di.filippo@gmail.com'>
+            <MagicButton
+              title='Send me an email'
+              icon={<MdOutlineEmail />}
+              position='right'
+            />
+          </a>
+          <a href='https://www.linkedin.com/in/dylandifilippo/'>
+            <MagicButton
+              title='Connect with me on LinkedIn'
+              icon={<FaLinkedin />}
+              position='right'
+            />
+          </a>
+        </div>
       </div>
       <div className='flex mt-16 md:flex-row flex-col justify-between items-center'>
         <p className='md:text-base text-sm md:font-normal font-light'>Copyright © {currentYear} Dylan Di Filippo</p>
